31b39374c69227772b6d0fb319d935b2c75826fb
lhuissier4 <127345152+lhuissier4@users.noreply.github.com>
2023-11-08 06:35:04 +0100

    add ls, nano, WIP cat

---
+++ cat.cmd
@@ -0,0 +1,27 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+set Nb_arg=0
+for %%a in (%*) do (
+    set /a Nb_arg += 1
+)
+
+if "%~1"=="" ( REM vérifie d'un nom est passé en argument
+    echo Erreur syntaxe attendu : %0 [NomDuFichier.extention]
+    exit /b 1
+) else if %Nb_arg% gtr 1 (
+    echo Erreur : Il y a trop d'arguments.
+    echo Syntaxe attendue : %0 [NomDuFichier.extension]
+    exit /b 1
+) else if "%~x1"=="" (  REM vérifie l'extention du fichier
+    echo Erreur syntaxe : %0 [NomDuFichier.extention]
+    exit /b 1
+) else (
+    if not exist %1 (
+        echo Erreur le fichier n'existe pas
+        exit /b 1
+    )
+    type %1
+    exit /b 0
+endlocal
---
+++ ls.cmd
@@ -0,0 +1,29 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Initialisation du nombre d'arguments
+set "Nb_arg=0"
+
+:: Compter le nombre d'arguments
+for %%a in (%*) do (
+    set /a Nb_arg += 1
+)
+
+:: Définition du chemin d'accès initial (répertoire actuel)
+set "chemin_dacces=%cd%"
+
+:: Vérification des arguments
+if %Nb_arg% gtr 0 (
+    if exist "%~1" (
+        set "chemin_dacces=%~1"
+    ) else (
+        echo Le chemin d'acces specifie n'existe pas.
+        exit /b 1
+    )
+)
+
+:: Affichage du contenu du répertoire
+dir "%chemin_dacces%"
+
+:: Fin du script
+endlocal
